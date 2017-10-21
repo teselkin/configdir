0235d7184f0cc5cadcbfce537e44c41e6f182048
Dmitry Teselkin <dteselkin@mirantis.com>
2017-10-21 23:29:31 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name="ConfigDir",
+    version="0.1",
+    packages=find_packages(),
+
+    author="Dmitry Teselkin",
+    author_email="teselkin.d@gmail.com",
+    description="Hierarchical config directory based on YAML",
+)
