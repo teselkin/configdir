0c863b10c0db46ecb5bb99907267dff538d619f1
Dmitry Teselkin <dteselkin@mirantis.com>
2017-10-23 16:55:32 +0300

    Update test.py

--- test.py
+++ test.py
@@ -2,5 +2,7 @@ from configdir import ConfigDir
 
 config = ConfigDir(root='example/config')
 config.open()
-print(str(config.dict(keystr='debian:debian')))
+print(str(config.load(keystr='')))
+print(str(config.load(keystr=':')))
+print(str(config.load(keystr='debian:debian')))
 print(str(config.get(keystr='debian:debian:package_manager')))
