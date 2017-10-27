23f8df5f6927fd3d7023f13ef25068c181d62be2
Dmitry Teselkin <dteselkin@mirantis.com>
2017-10-27 19:18:25 +0300

    Use requirements.txt in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="ConfigDir",
     version="0.2",
     packages=find_packages(),
+    install_requires=requirements,
 
     author="Dmitry Teselkin",
     author_email="teselkin.d@gmail.com",
